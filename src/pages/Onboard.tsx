b37db718438e80cf5afb8f628ce69769a196bb99
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-08-19 13:14:37 +0900

    Resized fonts, changed browser-tab icon

--- src/pages/Onboard.tsx
+++ src/pages/Onboard.tsx
@@ -17,13 +17,13 @@ const Onboard = () => {
         left={88}
         color={DEFAULTCOLOR}
       >
-        <Text fontSize="64px" lineHeight="76px">
+        <Text fontSize="64px" lineHeight="76px" letterSpacing="0.01em">
           <strong>키지</strong>와
         </Text>
-        <Text fontSize="64px" lineHeight="76px">
+        <Text fontSize="64px" lineHeight="76px" letterSpacing="0.01em">
           쉽고 편하게
         </Text>
-        <Text fontSize="64px" lineHeight="76px">
+        <Text fontSize="64px" lineHeight="76px" letterSpacing="0.01em">
           <strong>키오스크 연습</strong>해요!
         </Text>
       </Box>
@@ -33,7 +33,7 @@ const Onboard = () => {
         h={0}
         left={95}
         top={379}
-        border="3px solid #2C5282"
+        border="2px solid #2C5282"
       />
       <Box position="absolute" opacity="0.2" left={339} top={467}>
         <img src={Logo} width="407px" height="408px" />
@@ -48,18 +48,18 @@ const Onboard = () => {
       />
       <Text
         position="absolute"
-        w={227}
+        w={256}
         h={43}
         fontWeight={400}
         left={520}
         top={1046}
         color="#FFFFFF"
-        fontSize="36px"
+        fontSize="34px"
         lineHeight="43px"
         onClick={() => navigate('/home')}
         cursor="pointer"
       >
-        밀어서 시작하기 &gt;
+        밀어서 시작하기 {'>'}
       </Text>
     </Wrapper>
   );
