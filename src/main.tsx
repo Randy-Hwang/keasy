94f6fbe7f2ac51dbbae9e89ea91b0a484a769f60
Jeon Sangwan <maxswjeon@naver.com>
2022-09-08 17:56:15 +0900

    Move theme definition to theme.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './router';
 
 import './assets/global.css';
-
-const theme = extendTheme({
-  colors: {
-    main: '#2C5282',
-    text: '#121212',
-  },
-  fonts: { body: `Pretendard-Regular, sans-serif` },
-});
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
---
+++ src/theme.tsx
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    main: '#2C5282',
+    text: '#121212',
+  },
+  fonts: { body: `Pretendard-Regular, sans-serif` },
+});
+
+export default theme;
