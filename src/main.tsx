36d5550a61ef2330d2668159cbbe95f1da5ed253
Jeon Sangwan <maxswjeon@naver.com>
2022-09-08 17:49:03 +0900

    Delete fonts.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Fonts from './fonts';
 import App from './router';
 
 import './assets/global.css';
@@ -17,7 +16,6 @@ const theme = extendTheme({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <Fonts />
       <App />
     </ChakraProvider>
   </React.StrictMode>
